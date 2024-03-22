75a221c88fba90f621f3f00ef8730d50ce218ad2
khalil-rebhi <khalil.rebhi@esprit.tn>
2024-03-22 02:32:24 +0100

    Listing creation step 2 (Location selection, Map component, Country autocomplete)

---
+++ app/components/inputs/CountrySelect.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select';
+import { FlagIcon } from 'react-flag-kit';
+
+export type CountrySelectValue = {
+    value : string;
+    flag : string;
+    label : string;
+    latlng : number[];
+    region : string;
+}
+
+interface CountrySelectProps{
+    value?: CountrySelectValue;
+    onChange: (value: CountrySelectValue) => void
+}
+
+const CountrySelect : React.FC<CountrySelectProps> = ({
+    value,
+    onChange
+}) => {
+
+    const { getAll } = useCountries();
+    getAll().map((country) => console.log(country.value));
+  return (
+    <div>
+        <Select
+            placeholder = "Anywhere"
+            isClearable
+            options={getAll()}
+            value={value}
+            onChange={(value) => onChange(value as CountrySelectValue)}
+            formatOptionLabel={(option : any) => (
+                <div className="flex flex-row items-center gap-3">
+                    <div><FlagIcon code={option.value} /></div>
+                    <div>{option.label}, <span className='text-neutral-500 ml-1'>{option.region}</span></div>
+                </div>
+            )}
+            classNames={{
+                control: () => 'p-3 border-2',
+                input : () => 'text-lg',
+                option : () => 'text-lg'
+            }}
+            theme={(theme) => ({
+                ...theme,
+                borderRadius : 6,
+                colors:{
+                    ...theme.colors,
+                    primary: '#f43f5e',
+                    primary25: '#ffe4e6'
+                }
+            })}
+        />
+    </div>
+  )
+}
+export default CountrySelect;
